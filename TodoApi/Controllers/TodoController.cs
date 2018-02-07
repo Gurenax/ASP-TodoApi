a9231e142d257a2229051389e484be85282bc5da
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-07 21:45:43 +1100

    Add TodoItem DELETE route

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -78,5 +78,20 @@ namespace TodoApi.Controllers
             _context.SaveChanges();
             return new NoContentResult();
         }
+
+        // DELETE TodoItem
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
     }
 }
