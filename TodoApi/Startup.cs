7196776767f793afe0e7690cbc7031930b67047f
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-07 21:55:59 +1100

    Add Swagger interface

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 //using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TodoApi
 {
@@ -21,6 +22,12 @@ namespace TodoApi
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +37,15 @@ namespace TodoApi
             //{
             //    app.UseDeveloperExceptionPage();
             //}
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
